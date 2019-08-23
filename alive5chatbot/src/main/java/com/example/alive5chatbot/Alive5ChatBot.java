21b9a9f315939f8f2104850deb495befc87decb4
mubashirmarwat <mubashir.ali@goldevelopers.com>
2019-08-23 12:17:35 +0500

    adding web view activity

--- alive5chatbot/src/main/java/com/example/alive5chatbot/Alive5ChatBot.java
+++ alive5chatbot/src/main/java/com/example/alive5chatbot/Alive5ChatBot.java
@@ -1,29 +1,14 @@
 package com.example.alive5chatbot;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
 
-public class Alive5ChatBot extends AppCompatActivity {
+public class Alive5ChatBot {
+    public static void show(Context c,String message){
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alive5_chat_bot);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Intent intent = new Intent(c,WebView.class);
+        c.startActivity(intent);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
 }
