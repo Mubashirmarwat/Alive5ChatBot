cca05cf9859c2b6edff51198059c39e10572dc9c
mubashirmarwat <mubashir.ali@goldevelopers.com>
2019-08-23 12:04:00 +0500

    ading method

--- alive5chatbot/src/main/java/com/example/alive5chatbot/Alive5ChatBot.java
+++ alive5chatbot/src/main/java/com/example/alive5chatbot/Alive5ChatBot.java
@@ -1,11 +1,29 @@
 package com.example.alive5chatbot;
 
-import android.widget.Toast;
-import android.content.Context;
-public class Alive5ChatBot {
-    public static void s(Context c,String message){
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+public class Alive5ChatBot extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alive5_chat_bot);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }
