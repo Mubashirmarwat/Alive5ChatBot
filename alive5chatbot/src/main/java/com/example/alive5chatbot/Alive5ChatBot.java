0183bcad7dc3ca5bc0473224915c19e286fdbe9d
mubashirmarwat <mubashir.ali@goldevelopers.com>
2019-08-23 11:54:33 +0500

    ading method

---
+++ alive5chatbot/src/main/java/com/example/alive5chatbot/Alive5ChatBot.java
@@ -0,0 +1,11 @@
+package com.example.alive5chatbot;
+
+import android.widget.Toast;
+import android.content.Context;
+public class Alive5ChatBot {
+    public static void s(Context c,String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
